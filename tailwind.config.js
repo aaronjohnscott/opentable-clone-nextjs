1afa2ca206d854791f65a4cda86c57e5f01711e0
Aaron Scott <aaronjohnscott@gmail.com>
2023-03-14 09:40:22 -0700

    updated home page JSX

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontSize: { 
+      "2xsm": "0.625rem",
+      "xsm": "0.75rem",
+      "sm": "0.875rem",
+      "base": "1rem",
+      "lg": "1.125rem",
+      "xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "3.75rem",
+      "7xl": "4.5rem",
+    },
   },
   plugins: [],
 }
